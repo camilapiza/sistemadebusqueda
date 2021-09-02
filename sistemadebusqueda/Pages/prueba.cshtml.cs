8d74fde83c42ae1fda6e28650349a665e087e4dd
Groupon <Groupon@DESKTOP-N4DQOOK>
2021-09-02 01:06:08 -0400

    Agregar archivos de proyecto.

---
+++ sistemadebusqueda/Pages/prueba.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sistemadebusqueda.Pages
+{
+    public class pruebaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
